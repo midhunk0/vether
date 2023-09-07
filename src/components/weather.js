e998f81a61f3e51f72e14ee7bf3906be5c9a3000
midhunrajk <midhunrajk009@gmail.com>
2023-09-07 19:49:59 +0530

    responsive ok

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable react-hooks/exhaustive-deps */
 // @ts-nocheck
 import React, { useState, useEffect } from "react";
@@ -5,7 +6,7 @@ import React, { useState, useEffect } from "react";
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const WeatherApp = () => {
-    const [city, setCity] = useState("trivandrum");
+    const [city, setCity] = useState("malappuram");
     const [tempUnit, setTempUnit] = useState("°C");
     const [speedUnit, setSpeedUnit] = useState("kph");
     const [pressureUnit, setPressureUnit] = useState("mb");
@@ -21,7 +22,8 @@ const WeatherApp = () => {
             <button
                 key={unit}
                 onClick={() => setUnitCallback(unit)}
-                style={{ background: selectedUnit === unit ? "red" : "white", color: selectedUnit === unit ? "white" : "black", marginLeft:"10px", border:"1px solid black", borderRadius:"4px", padding:"10px"}}
+                className="unit-button"
+                style={{ background: selectedUnit === unit ? "red" : "white", color: selectedUnit === unit ? "white" : "black"}}
             >
                 {unit}
             </button>
@@ -54,38 +56,37 @@ const WeatherApp = () => {
     }
 
     const openUnits=()=>{
-        setOpen(!open)
+        setOpen(!open) 
     }
 
     return (
-<div className="weather-app">
-    <h1>Weather App</h1>
-    <div className="offcanvas-container">
-        <input type="text" placeholder="Enter city name" value={city} onChange={(e) => setCity(e.target.value)} />
-        <button className={`toggle-button ${open ? 'open' : ''}`} onClick={openUnits} >Open Units</button>
-        <div className={`offcanvas-menu ${open ? 'open' : ''}`} onClick={openUnits} >
-            <div className="offcanvas-content" onClick={(e) => e.stopPropagation()}>
-                <button className="close-button" onClick={openUnits}>Close Units</button>
-                <ul>
-                    <li>Temperature: {setUnit(['°C', '°F'], tempUnit, setTempUnit)}</li>
-                    <li>Speed: {setUnit(['kph', 'mph'], speedUnit, setSpeedUnit)}</li>
-                    <li>Pressure: {setUnit(['mb', 'in'], pressureUnit, setPressureUnit)}</li>
-                    <li>Precipitation:{' '}{setUnit(['mm', 'in'], precipitationUnit, setPrecipitationUnit)}</li>
-                    <li>Distance: {setUnit(['km', 'miles'], distanceUnit, setDistanceUnit)}</li>
-                </ul>
+    <div className="weather-app">
+        <h1>Weather App</h1>
+        <div className="offcanvas-div">
+            <input type="text" placeholder="Enter city name" value={city} onChange={(e) => setCity(e.target.value)} />
+            <button className="open-button" onClick={openUnits} >Open Units</button>
+            <div className={`offcanvas-menu ${open ? 'open' : ''}`} onClick={openUnits} >
+                <div className="offcanvas-content" onClick={(e) => e.stopPropagation()}>
+                    <button className="close-button" onClick={openUnits}>Close Units</button>
+                    <ul>
+                        Temperature<li>{setUnit(['°C', '°F'], tempUnit, setTempUnit)}</li>
+                        Speed<li>{setUnit(['kph', 'mph'], speedUnit, setSpeedUnit)}</li>
+                        Pressure<li>{setUnit(['mb', 'in'], pressureUnit, setPressureUnit)}</li>
+                        Precipitation<li>{setUnit(['mm', 'in'], precipitationUnit, setPrecipitationUnit)}</li>
+                        Distance<li>{setUnit(['km', 'miles'], distanceUnit, setDistanceUnit)}</li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-
 
 
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
         {data && (
             <div>
-                <h2>Details of {data.location.name}, {data.location.region}, {data.location.country}</h2>
-                <div className="current-div">                            
-                    <div className="mid-div">
+                <div className="current">                            
+                    <div className="div1">
+                        <h2>Details of {data.location.name}, {data.location.region}, {data.location.country}</h2>
                         <img src={data.current.condition.icon} alt="weather-img" style={{margin:"0 40px", width:"100px", height:"100px"}}/>
                         <p>{data.current.condition.text}</p>
                         {tempUnit === "°C" 
@@ -103,22 +104,18 @@ const WeatherApp = () => {
                             )
                         }
                     </div>
-                    <div className="mid-div">
-                        {speedUnit === "kph" 
-                            ? <p>Wind Speed: {data.current.wind_kph}kph</p>
-                            : <p>Wind Speed: {data.current.wind_mph}mph</p>
-                        }
-                        <p>Wind Direction: {data.current.wind_degree} {data.current.wind_dir}</p>
-                    </div>
-                    <div className="mid-div">
-                        <p>Latitudes: {data.location.lat}</p>
-                        <p>Logitudes: {data.location.lon}</p>
-                        <p>Humidity: {data.current.humidity}</p>
-                        <p>Clouds: {data.current.cloud}</p>
-                        <p>UV: {data.current.uv}</p>
-                    </div>
-                    <div className="main-sub-div">
-                        <div className="sub-div">
+                    <div className="div23">
+                        <div className="div2">
+                            {speedUnit === "kph" 
+                                ? <p>Wind Speed: {data.current.wind_kph}kph</p>
+                                : <p>Wind Speed: {data.current.wind_mph}mph</p>
+                            }
+                            <p>Wind Direction: {data.current.wind_degree} {data.current.wind_dir}</p>
+                            <p>Humidity: {data.current.humidity}</p>
+                            <p>Clouds: {data.current.cloud}</p>
+                            <p>UV: {data.current.uv}</p>
+                        </div>
+                        <div className="div3">
                             {pressureUnit === "mb" 
                                 ? <p>Pressure: {data.current.pressure_mb}mb</p>
                                 : <p>Pressure: {data.current.pressure_in}in</p>
@@ -127,8 +124,6 @@ const WeatherApp = () => {
                                 ? <p>Precipitation: {data.current.precip_mm}mm</p>
                                 : <p>Precipitation: {data.current.precip_in}in</p>
                             }
-                        </div>
-                        <div className="sub-div">
                             {distanceUnit === "km" 
                                 ? <p>Visual clarity: {data.current.vis_km}km</p> 
                                 : <p>Visual clarity: {data.current.vis_miles}miles</p>
@@ -142,7 +137,7 @@ const WeatherApp = () => {
                 </div>
                 <h3>7 Day Forecast</h3>
                 {data && data.forecast && data.forecast.forecastday && (
-                    <div style={{display:"flex", overflowX:"auto", gap:"10px"}}>
+                    <div style={{display:"flex", overflowX:"auto", gap:"20px"}}>
                         {data.forecast.forecastday.slice(0, 7).map((day, index) => (
                         <div key={index} style={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center",gap:"10px", border:"1px solid black", borderRadius:"16px", padding:"10px", minWidth:"300px"}}>
                             <p>{day.date}</p>
@@ -151,20 +146,20 @@ const WeatherApp = () => {
                             {tempUnit === "°C" 
                                 ? (
                                     <>
-                                        <p>Maximum Temperature: {day.day.maxtemp_c}°C</p>
-                                        <p>Minimum Temperature: {day.day.mintemp_c}°C</p>
+                                        {/* <p>Maximum Temperature: {day.day.maxtemp_c}°C</p>
+                                        <p>Minimum Temperature: {day.day.mintemp_c}°C</p> */}
                                         <p>Average Temperature: {day.day.avgtemp_c}°C</p>
                                     </>
                                 ) 
                                 : (
                                     <>
-                                        <p>Maximum Temperature: {day.day.maxtemp_f}°F</p>
-                                        <p>Minimum Temperature: {day.day.mintemp_f}°F</p>
+                                        {/* <p>Maximum Temperature: {day.day.maxtemp_f}°F</p>
+                                        <p>Minimum Temperature: {day.day.mintemp_f}°F</p> */}
                                         <p>Average Temperature: {day.day.avgtemp_f}°F</p>
                                     </>
                                 )
                             }
-                            {speedUnit === "kph" 
+                            {/* {speedUnit === "kph" 
                                 ? <p>Maximum Wind Speed: {day.day.maxwind_kph}kph</p>
                                 : <p>Maximum Wind Speed: {day.day.maxwind_mph}mph</p>
                             }
@@ -184,14 +179,14 @@ const WeatherApp = () => {
                             <p>Sunrise and Sunset: {day.astro.sunrise} and {day.astro.sunset}</p>
                             <p>Moonrise and Moonset: {day.astro.sunrise} and {day.astro.sunset}</p>
                             <p>Moon Phase: {day.astro.moon_phase}</p>
-                            <p>Moon Illumination: {day.astro.moon_illumination}</p>
+                            <p>Moon Illumination: {day.astro.moon_illumination}</p> */}
                         </div>
                         ))}
                     </div>
                 )}
                 <h3>Hourly Forecast</h3>
                 {data && data.forecast && data.forecast.forecastday && (
-                    <div style={{display:"flex", overflowX:"auto", gap:"10px"}}>
+                    <div style={{display:"flex", overflowX:"auto", gap:"20px"}}>
                         {data.forecast.forecastday[0].hour.map((hour, index) => (
                             <div key={index} style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center",gap:"10px", border:"1px solid black", borderRadius:"16px", padding:"10px", minWidth:"300px"}}>
                                 <p>Time: {hour.time.split(' ')[1]}</p>
@@ -201,27 +196,26 @@ const WeatherApp = () => {
                                     ? (
                                         <>
                                             <p>Temperature: {hour.temp_c}°C</p>
-                                            <p>Feels Like: {hour.feelslike_c}°C</p>
+                                            {/* <p>Feels Like: {hour.feelslike_c}°C</p>
                                             <p>Wind Chill: {hour.windchill_c}°C</p>
                                             <p>Heat Index: {hour.heatindex_c}°C</p>
-                                            <p>Dew Point: {hour.dewpoint_c}°C</p>
+                                            <p>Dew Point: {hour.dewpoint_c}°C</p> */}
                                         </>
                                     ) 
                                     : (
                                         <>
                                             <p>Temperature: {hour.temp_f}°F</p>
-                                            <p>Feels Like: {hour.feelslike_f}°F</p>
+                                            {/* <p>Feels Like: {hour.feelslike_f}°F</p>
                                             <p>Wind Chill: {hour.windchill_f}°F</p>
                                             <p>Heat Index: {hour.heatindex_f}°F</p>
-                                            <p>Dew Point: {hour.dewpoint_f}°F</p>                                                
+                                            <p>Dew Point: {hour.dewpoint_f}°F</p>                                                 */}
                                         </>
                                     )
                                 }
-                                {speedUnit === "kph" 
+                                {/* {speedUnit === "kph" 
                                     ? <p>Wind Speed: {hour.wind_kph}kph</p>
                                     : <p>Wind Speed: {hour.wind_mph}mph</p>
                                 }
-                                
                                 <p>Wind Degree: {hour.wind_degree}</p>
                                 <p>Wind Direction: {hour.wind_dir}</p>
                                 {pressureUnit === "mb" 
@@ -244,17 +238,17 @@ const WeatherApp = () => {
                                     ? <p>Gust Speed: {hour.gust_kph}kph</p>
                                     : <p>Gust Speed: {hour.gust_mph}mph</p>
                                 }
-                                <p>UV: {hour.uv}</p>
+                                <p>UV: {hour.uv}</p> */}
                             </div>
                         ))}
                     </div>
                 )}
             </div>
 
-)}
-</div>
-    );
-};
+                )}
+            </div>
+            );
+        };
 
 export default WeatherApp;
 
